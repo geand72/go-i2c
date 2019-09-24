41cc68b907309d892f76a5f4528a98b26a2ce19f
Andrey Gerasimov <geand72@gmail.com>
2019-09-24 10:28:16 +0300

    change logging to error level

--- logger.go
+++ logger.go
@@ -5,6 +5,7 @@ import logger "github.com/d2r2/go-logger"
 // You can manage verbosity of log output
 // in the package by changing last parameter value.
 var lg = logger.NewPackageLogger("i2c",
-	logger.DebugLevel,
+	logger.ErrorLevel,
+	//logger.DebugLevel,
 	// logger.InfoLevel,
 )
